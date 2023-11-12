da4319979445d54f9709a9fd48cde55908bfecd1
Anob Chakrabarti <anob.chakrabarti@crick.ac.uk>
2023-11-12 20:01:36 +0000

    updated org help

--- bin/remove_random_priming.R
+++ bin/remove_random_priming.R
@@ -29,7 +29,7 @@ if(length(opt) != 11) {
 
 if(!opt$org %in% c("hg38", "mm10", "mm39", "rn6", "rn7")) {
   print_help(opt_parser)
-  stop("--org should be one of [human, mouse, rat].")
+  stop("--org should be one of [hg38, mm10, mm39, rn6, rn7].")
 }
 
 # ==========
